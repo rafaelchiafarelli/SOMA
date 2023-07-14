1cf0fb86be2d7998b34a25e9d3cd2a4deee01955
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2023-07-14 17:56:39 -0300

    must run as sudo

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-sudo apt install -y python3.8
-sudo mkdir /etc/SOMA
-sudo cp -r ../SOMA/* /etc/SOMA/
+apt install -y python3.8
+
+cp -r ../SOMA/* /etc/SOMA/
 echo "installing dependencies"
-sudo su
+
 cd /etc/SOMA/
 
 python3.8 -m venv venv
@@ -16,6 +16,6 @@ echo "patch mediapipe"
 cp ./assets/face_mesh_connections.py ./venv/lib/python3.8/site-packages/mediapipe/python/solutions/face_mesh_connections.py
 cp ./assets/holistic.py ./venv/lib64/python3.8/site-packages/mediapipe/python/solutions/holistic.py
 
-sudo cp /etc/SOMA/assets/manager.service /etc/systemd/system/manager.service
+cp /etc/SOMA/assets/manager.service /etc/systemd/system/manager.service
 
 
